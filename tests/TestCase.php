08a71820eb56e1f31cc2083e744abd0256fd31c8
Robert Sipek <rob.sipek@gmail.com>
2020-04-16 14:14:19 +0200

    Develop files

---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Opendevel\Aws\SnsSes;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+class TestCase extends PHPUnitTestCase
+{
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Opendevel\Aws\SnsSes;
+
+use function error_reporting;
+
+use const E_ALL;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
